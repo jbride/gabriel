18969fbfda95feb60a0b4de73869987aeb703ed7
jbride <jbride2001@yahoo.com>
2024-10-29 20:43:41 -0600

    sqlite persistence:  now using connection pool

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,19 +1,22 @@
 use std::env;
 
-use rusqlite::Connection;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
 
 use crate::block::BlockAggregateOutput;
 
 #[derive(Debug)]
 pub struct SQLitePersistence {
-    sql_conn: Connection
+    pool: Pool<SqliteConnectionManager>
 }
 
 impl SQLitePersistence {
     pub fn new() -> anyhow::Result<(Self)> {
         let sqlite_absolute_path = env::var("SQLITE_ABSOLUTE_PATH")?;
-        let sql_conn = Connection::open(&sqlite_absolute_path)?;
+        let manager = SqliteConnectionManager::file(&sqlite_absolute_path);
+        let pool = r2d2::Pool::builder().max_size(15).build(manager).unwrap();
 
+        let sql_conn = pool.get().unwrap();
         sql_conn.execute(
             "create table if not exists p2pk_utxo_block_aggregates (
                  block_height integer not null,
@@ -30,14 +33,16 @@ impl SQLitePersistence {
         );
 
         Ok(SQLitePersistence {
-            sql_conn
+            pool
         })
     }
 
     pub fn persist_block_aggregates(&self, block_aggregate: &BlockAggregateOutput) -> anyhow::Result<(usize)> {
         
         let sql = "INSERT INTO p2pk_utxo_block_aggregates VALUES(?1,?2,?3,?4,?5)";
-        let db_exec_results = self.sql_conn.execute(sql, [
+
+        let sql_conn = self.pool.get().unwrap();
+        let db_exec_results = sql_conn.execute(sql, [
             block_aggregate.block_height.to_string(),
             block_aggregate.block_hash_big_endian.clone(),
             block_aggregate.date.clone(),
